0659a568b5ceb304c1aaed5f26238042f4abb67a
kdsama <kdsama5593@gmail.com>
2023-10-23 17:20:11 +0100

    added shell command file

--- internal/modules/shell.go
+++ internal/modules/shell.go
@@ -1,6 +1,21 @@
 package modules
 
-func NewShell(shellMap map[string]interface{}) (string, error) {
+import "fmt"
 
-	return "", nil
+func NewShell(shellMap map[string]interface{}) ([]string, error) {
+
+	var cmds []string
+	if _, ok := shellMap["chdir"]; ok {
+		cmds = append(cmds, fmt.Sprintf("cd %s", shellMap["chdir"].(string)))
+	}
+	if _, ok := shellMap["cmd"]; !ok {
+		return cmds, fmt.Errorf("command %w", ErrNotFound)
+	}
+
+	if _, ok := shellMap["executable"]; ok {
+		cmds = append(cmds, fmt.Sprintf("chsh -s %s", shellMap["executable"].(string)))
+	}
+	cmds = append(cmds, shellMap["cmd"].(string))
+
+	return cmds, nil
 }
