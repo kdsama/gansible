670d62bf4216cac59c72c812464427673c03b8ce
kdsama <kdsama5593@gmail.com>
2023-10-28 19:12:09 +0100

    added the same for linear strategy, next would need to converge the results

--- internal/engine.go
+++ internal/engine.go
@@ -1,7 +1,6 @@
 package internal
 
 import (
-	"fmt"
 	"sync"
 )
 
@@ -9,6 +8,8 @@ type Engine struct {
 	inventory     *MainInventory
 	playbook      *PlayBook
 	maxConcurrent int
+	wg            *sync.WaitGroup
+	sshCache      map[string]*sshConn
 }
 
 func NewEngine(playbookPath string, hostPath string) *Engine {
@@ -30,32 +31,38 @@ func NewEngine(playbookPath string, hostPath string) *Engine {
 		inventory:     inventory,
 		playbook:      pb,
 		maxConcurrent: 10,
+		wg:            &sync.WaitGroup{},
+		sshCache:      map[string]*sshConn{},
 	}
 }
 
 func (e *Engine) Run() {
 	// os := "ubuntu"
-	cache := map[string]*sshConn{}
 
 	for i := range e.playbook.Plays {
 		// fmt.Println("O ERROR", o.Err, strings.Trim(o.Err, " "))
 		switch e.playbook.Plays[i].Strategy {
 		case "free":
-			e.FreeStrategy(i, cache)
+			e.FreeStrategy(i)
 		default:
-			e.LinearStrategy(i, cache)
+			e.LinearStrategy(i)
 		}
 
 	}
 }
 
-func (e *Engine) LinearStrategy(i int, cache map[string]*sshConn) {
+func (e *Engine) LinearStrategy(i int) {
 	wg := sync.WaitGroup{}
 	respObj := e.playbook.Generate(i)
-
+	if e.playbook.Plays[i].Serial > 0 {
+		e.maxConcurrent = e.playbook.Plays[i].Serial
+	}
 	for _, h := range respObj.hosts {
-		obj := e.inventory.inv.All.Hosts[h]
-		cache[h] = NewSshConn(obj.AnsibleHost, obj.AnsibleUser, obj.AnsibleSshPass, "", obj.AnsiblePort)
+		if _, ok := e.sshCache[h]; ok {
+			obj := e.inventory.inv.All.Hosts[h]
+			e.sshCache[h] = NewSshConn(obj.AnsibleHost, obj.AnsibleUser, obj.AnsibleSshPass, "", obj.AnsiblePort)
+		}
+
 	}
 	// So  I need to run this with x concurrency
 	// If its length is less than x, we run as is
@@ -65,29 +72,33 @@ func (e *Engine) LinearStrategy(i int, cache map[string]*sshConn) {
 	// 0:11
 	//11:21
 	//21:22
+	for k := 0; k < len(respObj.hosts)/e.maxConcurrent; k += e.maxConcurrent {
+		start, end := k*e.maxConcurrent, ((k + 1) * e.maxConcurrent)
+		if end > len(respObj.hosts) {
+			end = len(respObj.hosts)
+		}
+		for _, t := range respObj.tasks {
+			wg.Add(len(respObj.hosts))
 
-	for _, t := range respObj.tasks {
-		wg.Add(len(respObj.hosts))
+			for _, h := range respObj.hosts[start:end] {
+				h := h
+				t := t
+				go func() {
+					defer wg.Done()
+					for _, c := range t.cmds {
+						e.sshCache[h].execute(c)
+					}
 
-		for _, h := range respObj.hosts {
-			h := h
-			t := t
-			go func() {
-				defer wg.Done()
-				for _, c := range t.cmds {
-					cache[h].execute(c)
-				}
-
-			}()
+				}()
+			}
 		}
 	}
 
-	fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 	wg.Wait()
 
 }
 
-func (e *Engine) FreeStrategy(i int, cache map[string]*sshConn) {
+func (e *Engine) FreeStrategy(i int) {
 	wg := sync.WaitGroup{}
 	respObj := e.playbook.Generate(i)
 	if e.playbook.Plays[i].Serial > 0 {
@@ -95,7 +106,7 @@ func (e *Engine) FreeStrategy(i int, cache map[string]*sshConn) {
 	}
 	for _, h := range respObj.hosts {
 		obj := e.inventory.inv.All.Hosts[h]
-		cache[h] = NewSshConn(obj.AnsibleHost, obj.AnsibleUser, obj.AnsibleSshPass, "", obj.AnsiblePort)
+		e.sshCache[h] = NewSshConn(obj.AnsibleHost, obj.AnsibleUser, obj.AnsibleSshPass, "", obj.AnsiblePort)
 	}
 	wg.Add(len(respObj.hosts))
 	for k := 0; k < len(respObj.hosts)/e.maxConcurrent; k += e.maxConcurrent {
@@ -110,7 +121,7 @@ func (e *Engine) FreeStrategy(i int, cache map[string]*sshConn) {
 				for _, t := range respObj.tasks {
 					h := h
 					for _, c := range t.cmds {
-						cache[h].execute(c)
+						e.sshCache[h].execute(c)
 					}
 
 				}
